6a1e2297105eea915e0b0c7d1ab7935382bb6518
Nisschhal <mrnischalpuri@gmail.com>
2024-10-03 07:37:38 +0545

    validation added with type

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,59 @@
+// validate interface and function for user inputs
+interface Validatable {
+  value: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInputs: Validatable) {
+  // if is required is on
+  let isValid = true;
+  // if string check if length is not equal to 0
+  if (validatableInputs.required) {
+    isValid = isValid && validatableInputs.value.trim().toString().length !== 0;
+  }
+
+  // check string minlength
+  if (
+    validatableInputs.minLength != null &&
+    typeof validatableInputs.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInputs.value.length >= validatableInputs.minLength;
+  }
+
+  // check string maxlenfth
+  if (
+    validatableInputs.maxLength != null &&
+    typeof validatableInputs.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInputs.value.length <= validatableInputs.maxLength;
+  }
+
+  // check min number
+  if (
+    validatableInputs.min != null &&
+    typeof validatableInputs.value === "number"
+  ) {
+    isValid = isValid && +validatableInputs.value >= validatableInputs.min;
+  }
+
+  // check max number
+  if (
+    validatableInputs.max != null &&
+    typeof validatableInputs.value === "number"
+  ) {
+    isValid = isValid && +validatableInputs.value <= validatableInputs.max;
+  }
+
+  //return result
+  return isValid;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -62,25 +118,42 @@ class ProjectInput {
   // form submithandler function
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.gatherUserInputs());
+    const userInput = this.gatherUserInputs();
+    if (Array.isArray(userInput)) {
+      console.log(this.gatherUserInputs());
+      this.clearInput();
+    }
   }
 
   // gather the user inputs
   private gatherUserInputs(): [string, string, number] | void {
     const title = this.titleInputElement.value;
+    const validateTitle: Validatable = {
+      value: title,
+      required: true,
+    };
     const description = this.descriptionElement.value;
+    const validateDescription: Validatable = {
+      value: description,
+      required: true,
+      minLength: 5,
+    };
     const people = this.peopleInputElement.value;
-
+    const validatePeople: Validatable = {
+      value: description,
+      required: true,
+      min: 1,
+      max: 10,
+    };
     //check if empty
     if (
-      title.trim().length == 0 ||
-      description.trim().length === 0 ||
-      people.trim().length == 0
+      !validate(validateTitle) ||
+      !validate(validateDescription) ||
+      !validate(validatePeople)
     ) {
       alert("Invalid Input, please try again!");
       return;
     } else {
-      this.clearInput();
       return [title, description, +people];
     }
   }
